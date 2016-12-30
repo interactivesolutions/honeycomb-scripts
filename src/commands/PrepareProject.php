feb054b3fb778e15aa54a490c3ecbdd3deb9fb8f
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2016-12-30 13:06:51 +0200

    Added prepare project command

---
+++ src/commands/PrepareProject.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace interactivesolutions\honeycombscripts\commands;
+
+use interactivesolutions\honeycombcore\commands\HCCommand;
+
+class PrepareProject extends HCCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:hcproject';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deleting default laravel project files and creating hc structure';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->removeDefaultStructure();
+    }
+
+    /**
+     * Removing default structure of application
+     */
+    private function removeDefaultStructure()
+    {
+        $confirm = $this->confirm('Are you sure? It will delete some directories with all files in them.)');
+
+        if ($confirm)
+        {
+            $this->deleteDirectory('app/Http/Controllers', true);
+            $this->deleteDirectory('app/Http/Console', true);
+            $this->deleteDirectory('app/routes', true);
+            $this->deleteDirectory('routes', true);
+
+            $this->createDirectory('app/Http/Controllers');
+            $this->createDirectory('app/Http/Console');
+            $this->createDirectory('app/routes');
+
+            $this->createFileFromTemplate([
+                "destination" => 'app/Http/Console/Kernel.php',
+                "templateDestination" => __DIR__ . '/templates/app.console.kernel.template.txt',
+            ]);
+        }
+    }
+}
