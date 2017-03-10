232595c7c13aa5440c5b78f33582118e69fce301
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-03-10 10:39:11 +0200

    Updated routes generation
    Whene in development enviroment generates routes for all of the
    pacakges, which are not in vendor directory

--- src/app/commands/HCRoutes.php
+++ src/app/commands/HCRoutes.php
@@ -34,18 +34,40 @@ class HCRoutes extends HCCommand
         else
             $rootDirectory = '';
 
-        $files = $this->file->allFiles ($rootDirectory . 'app/routes');
+        if ($rootDirectory == '')
+            if (app ()->environment () == 'local') {
+
+                $files = $this->getConfigFiles ();
+
+                foreach ($files as $file)
+                    if (strpos ($file, '/vendor/') === false)
+                        $this->generateRoutes (realpath(implode ('/', array_slice (explode ('/', $file), 0, -3))) . '/');
+            }
+    }
+
+    /**
+     * Generating final routes file for package
+     *
+     * @param $directory
+     */
+    private function generateRoutes ($directory)
+    {
+        if (!file_exists ($directory . 'app/routes/'))
+            return;
+
+        $files = $this->file->allFiles ($directory . 'app/routes');
 
         $finalContent = '<?php' . "\r\n";
 
         foreach ($files as $file) {
+
             $finalContent .= "\r\n";
-            $finalContent .= '//' . (string)$file;
+            $finalContent .= '//' . implode ('/', array_slice(explode ('/', $file), -6)) . "\r\n";
             $finalContent .= str_replace ('<?php', '', $this->file->get ((string)$file)) . "\r\n";
         }
 
-        $this->file->put ($rootDirectory . HCRoutes::ROUTES_PATH, $finalContent);
+        $this->file->put ($directory . HCRoutes::ROUTES_PATH, $finalContent);
 
-        $this->comment ($rootDirectory . HCRoutes::ROUTES_PATH . ' file generated');
+        $this->comment ($directory . HCRoutes::ROUTES_PATH . ' file generated');
     }
 }
