85d1df804a742af66ad72186ea5e7650618c3ba1
Mantas Paulauskas <mantopastas@gmail.com>
2017-07-19 09:19:45 +0300

    added route folder creation

--- src/app/commands/service/HCServiceRoutes.php
+++ src/app/commands/service/HCServiceRoutes.php
@@ -3,6 +3,7 @@
 namespace interactivesolutions\honeycombscripts\app\commands\service;
 
 use DB;
+use File;
 use FilesystemIterator;
 use stdClass;
 
@@ -21,6 +22,8 @@ class HCServiceRoutes extends HCBaseServiceCreation
      */
     public function optimize (stdClass $data)
     {
+        $this->createRouteFolders($data);
+
         $fi = new FilesystemIterator($data->rootDirectory . 'app/routes/admin/', FilesystemIterator::SKIP_DOTS);
         $count = str_pad(iterator_count($fi) + 1, 2, '0', STR_PAD_LEFT) . '_';
 
@@ -85,5 +88,19 @@ class HCServiceRoutes extends HCBaseServiceCreation
         ]);
 
         return $destination;
+    }
+
+    /**
+     * @param stdClass $data
+     */
+    protected function createRouteFolders(stdClass $data)
+    {
+        if( ! File::exists($data->rootDirectory . 'app/routes/admin') ) {
+            $this->createDirectory($data->rootDirectory . 'app/routes/admin');
+        }
+
+        if( ! File::exists($data->rootDirectory . 'app/routes/api') ) {
+            $this->createDirectory($data->rootDirectory . 'app/routes/api');
+        }
     }
 }
